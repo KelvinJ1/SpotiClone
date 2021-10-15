97fb6760f2e80417b7566d6f4c715661fe96ccfa
Kelvin <kelvin03pm@gmail.com>
2021-10-14 23:08:24 -0500

    Scaffolding, modules, lazyLoading, paths, and sidebar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path:"", loadChildren:()=> import("./modules/home/home.module").then(m => m.HomeModule) 
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { MediaPlayerComponent } from './components/media-player/media-player.component';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
+
+
+
+@NgModule({
+  declarations: [
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent
+  ],
+  imports: [
+    CommonModule
+  ], 
+  exports:[
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent
+  ]
+})
+export class SharedModule { }
